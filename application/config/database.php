1494ad8aa3b610a0775086b4b1738ea4c6f8dc8c
Adriele de Moraes <146091307+DriMoraes01@users.noreply.github.com>
2024-04-09 15:34:38 -0300

    ajustes no projeto

--- application/config/database.php
+++ application/config/database.php
@@ -98,25 +98,4 @@ $db['default'] = array(
 
 
 
-/*$db['default'] = array(
-	'dsn'	=> '',
-	'port' => '5432',
-	'hostname' => '',
-	'username' => 'postgres',
-	'password' => '@ema*20!',
-	'database' => 'contas',
-	'dbdriver' => 'postgre',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => TRUE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE 
-);*/
+
--- application/views/login/index.php
+++ application/views/login/index.php
@@ -4,7 +4,7 @@
             <div class="container-fluid h-100">
                 <div class="row flex-row h-100 bg-white">
                     <div class="col-xl-8 col-lg-6 col-md-5 p-0 d-md-block d-lg-block d-sm-none d-none">
-                        <div class="lavalite-bg" style="background-image: url(<?= base_url('public/img/auth/login.jpg') ?>)">
+                        <div class="lavalite-bg" style="background-image: url(<?= base_url('assets/img/auth/login.jpg') ?>)">
                             <div class="lavalite-overlay"></div>
                         </div>
                     </div>
