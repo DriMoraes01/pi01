49cc2226c5189d295c6b21233a2714b45f205a54
Adriele de Moraes <146091307+DriMoraes01@users.noreply.github.com>
2024-04-30 09:28:51 -0300

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://localhost/accountsystem/';
-$config['base_url'] = 'http://localhost/pi/pi01/';
+//$config['base_url'] = 'http://localhost/helponpets/';
+$config['base_url'] = 'https://helponpets.000webhostapp.com/';
 /*
 |--------------------------------------------------------------------------
 | Index File
